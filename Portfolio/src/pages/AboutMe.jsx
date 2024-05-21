7c39e424028bc55ef2babb1568bd45654435be14
HaleighElkins <152942336+HaleighElkins@users.noreply.github.com>
2024-05-20 19:25:41 -0600

    started about me jsx

---
+++ Portfolio/src/pages/AboutMe.jsx
@@ -0,0 +1,27 @@
+// Profilie pic?
+
+
+
+
+export default function AboutMe() {
+    return(
+    <div className="container pt-4">
+        <h2>About Me</h2>
+        <div id='about-me'>
+            <div className='cal-3 mb-4'>
+                {/* image */}
+            </div>
+    <p className='paragraph'>
+        {/* Welcome */}
+    </p>
+    <p className='paragraph'>
+        {/* About me */}
+    </p>
+    <p className='paragraph'>
+        {/* Thank yous */}
+    </p>
+        </div>
+    </div>
+
+    );
+}
