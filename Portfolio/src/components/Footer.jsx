06582bbb9a6f7167efefbe3bbfb1bba54f60aafb
HaleighElkins <152942336+HaleighElkins@users.noreply.github.com>
2024-05-20 21:06:18 -0600

    added in footer for portfolio, incuding my svgs that link to my profilies

---
+++ Portfolio/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import githubLogo from '../assets/svg/github.svg';
+import linkedLogo from '../assets/linkedin.svg';
+
+export default function Footer() {
+    const socialLinks = [
+      { url: 'https://www.linkedin.com/in/haleigh-elkins-9b38792b7/', logo: linkedLogo, alt: "LinkedIn logo", className: "logo linkedIn" },
+      { url: 'https://github.com/HaleighElkins', logo: githubLogo, alt: "Github logo", className: "logo github" }
+    ];
+  
+    return (
+      <div className="container-fluid text-center p-4">
+        {socialLinks.map((link, index) => (
+          <a key={index} className="m-4" href={link.url} target="_blank" rel="noopener noreferrer">
+            <img src={link.logo} className={link.className} alt={link.alt} />
+          </a>
+        ))}
+      </div>
+    );
+  }
+  
