12b1b2bba15240f9c5414c1f023b910e3662be27
HaleighElkins <152942336+HaleighElkins@users.noreply.github.com>
2024-05-22 21:37:03 -0600

    fixed some things on my about me

--- Portfolio/src/pages/AboutMe.jsx
+++ Portfolio/src/pages/AboutMe.jsx
@@ -16,21 +16,21 @@ export default function AboutMe() {
                 id='profile-pic'
                 />
             </div>
-    <p className='paragraph'>
+    <p className='paragraph'> 
         {/* Welcome */}
-        Welcome to my portfolio! Here you&#39ll find my resume, a collection of small projects, and information about me. Feel free to explore and don&#39t hesitate to reach out through the contact section.
+        Welcome to my portfolio! Here you&#39;ll find my resume, a collection of small projects, and information about me. Feel free to explore and don&#39;t hesitate to reach out through the contact section.
     </p>
     <p className='paragraph'>
         {/* About me */}
-        Hi there! I&#39m Haleigh, a 31-year-old tech enthusiast living in beautiful Colorado with my two adorable golden retrievers. I&#39m on the exciting journey of transitioning into a new career in programming. Currently, I&#39m about to complete a fullstack coding bootcamp, which has ignited my passion for all things tech.
+        Hi there! I&#39;m Haleigh, a 31-year-old tech enthusiast living in beautiful Colorado with my two adorable golden retrievers. I&#39;m on the exciting journey of transitioning into a new career in programming. Currently, I&#39;m about to complete a fullstack coding bootcamp, which has ignited my passion for all things tech.
 
         I love diving deep into how computers work and am constantly amazed by the endless possibilities of coding. My next adventure? Exploring the world of cybersecurity through further education.
 
-        When I&#39m not coding or learning about new technologies, you can find me exploring the great outdoors with my furry companions or tinkering with the latest gadgets. I&#39m thrilled about the future and can&#39t wait to make my mark in the tech world. Let&#39s connect and see what we can create together!
+        When I&#39;m not coding or learning about new technologies, you can find me exploring the great outdoors with my furry companions or tinkering with the latest gadgets. I&#39;m thrilled about the future and can&#39;t wait to make my mark in the tech world. Let&#39;s connect and see what we can create together!
     </p>
     <p className='paragraph'>
         {/* Thank yous */}
-        Thank you for visiting my portfolio! I hope you enjoyed exploring my projects and learning a bit about me. If you have any questions, ideas, or just want to connect, feel free to reach out through the contact section. I&#39m excited about the future and look forward to the possibilities ahead!
+        Thank you for visiting my portfolio! I hope you enjoyed exploring my projects and learning a bit about me. If you have any questions, ideas, or just want to connect, feel free to reach out through the contact section. I&#39;m excited about the future and look forward to the possibilities ahead!
     </p>
         </div>
     </div>
